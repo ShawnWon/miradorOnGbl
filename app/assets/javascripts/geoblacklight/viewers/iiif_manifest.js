16ab0a2d2492d74f5ddb4366284f0e6521ef7889
vagrant <vagrant@localhost.localdomain>
2021-03-13 04:42:02 +0000

    First commit

---
+++ app/assets/javascripts/geoblacklight/viewers/iiif_manifest.js
@@ -0,0 +1,42 @@
+
+
+//= require geoblacklight/viewers/viewer
+
+GeoBlacklight.Viewer.IiifManifest = GeoBlacklight.Viewer.extend({
+  load: function() {
+    var manifest_uri = document.getElementById('map').getAttribute('data-url');
+
+    var miradorInstance = Mirador.viewer({
+       id: 'map',
+       themes: {
+         light: {
+           palette: {
+             type: 'light',
+             primary: {
+               main: '#0088ce',
+             },
+           },
+         },
+       },
+       windows: [{
+         imageToolsEnabled:true,
+         manifestId: manifest_uri,
+         thumbnailNavigationPosition: 'far-bottom',
+       }],
+       window: {
+         hideSearchPanel: false,
+         hideWindowTitle: true,
+         hideAnnotationsPanel: true,
+         allowClose: false,
+         allowMaximize: false,
+         allowFullscreen: true,
+       },
+       workspace: {
+         showZoomControls: true,
+       },
+       workspaceControlPanel: {
+         enabled: false,
+       }
+     },[miradorImageToolsPlugin]);
+  }
+});
